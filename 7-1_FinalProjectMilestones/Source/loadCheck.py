fce2d8fd58475a285a0c3adceb9be4649bb31032
Ada <135479802+pardofelisu@users.noreply.github.com>
2025-11-22 19:28:11 -0500

    Create loadCheck.py

---
+++ 7-1_FinalProjectMilestones/Source/loadCheck.py
@@ -0,0 +1,6 @@
+#this is what loads first in the exe
+#checks that all shaders and textures are loaded
+#initiates glew/glfw/OpenGL download if needed
+#will also count time it took to load/exit program
+
+#wip file
